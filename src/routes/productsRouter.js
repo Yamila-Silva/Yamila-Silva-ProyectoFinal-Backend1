518a92ce21e4df04147cb6a55053ca9b691d9f05
Yamila Silva <lic.silvayasin@gmail.com>
2024-10-30 03:22:14 +0000

    Actualizacion

--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -8,10 +8,10 @@ ProductsManager.setPath("./src/data/productos.json")
 
 router.get('/', async (req, res) => {
     try {
-        const products = await ProductsManager.getProducts()
-        res.render('index', { products })
+        const products = await ProductsManager.getProducts();
+        res.status(200).json(products); // Envía los productos en formato JSON con estado 200
     } catch (error) {
-        console.error('Error al cargar los productos:', error)
-        res.status(500).send('Error al cargar los productos')
+        console.error('Error al cargar los productos:', error);
+        res.status(500).json({ error: 'Error al cargar los productos' });
     }
 });
